bfc25f2f6e73ac5eae735555796ea99d6628e92e
Sourav Das <sourav.das@hyland.com>
2025-08-09 16:01:24 +0530

    Add support for books

---
+++ my-books/Data/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace my_books.Data.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public required string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public required string Author { get; set; }
+        public string CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
+
